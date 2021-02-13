32281acdfd87555212771184da0e01e59c0bbe03
Ryley <ryley.g@hotmail.com>
2021-02-12 23:04:17 -0700

    added logging

--- EggTimerBot.py
+++ EggTimerBot.py
@@ -1,2 +1,22 @@
+# EggTimerBot.py
 import discord
+import os
+from dotenv import load_dotenv
+import logging
 
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(Levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
