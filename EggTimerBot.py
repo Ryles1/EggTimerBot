4a2d5b0e520f095ec75a560cfcc4ac95b5bd69ef
Ryley <ryley.g@hotmail.com>
2021-02-17 22:48:18 -0700

    preparations for voice

--- EggTimerBot.py
+++ EggTimerBot.py
@@ -13,12 +13,29 @@ logger.addHandler(handler)
 
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
+VOICE_CHANNEL_NAME = os.getenv('VOICE_CHANNEL_NAME')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    # get list of guilds that bot is a member
+    guilds = client.guilds
+
+    # get list of member objects from list of guilds
+    member_obj_list = [member for guild in guilds for member in guild.members]
+
+    # get voice channel for checking state and if anyone is in it
+    voice_channels = [chan for guild in guilds for chan in guild.voice_channels]
+    voice_chan_obj = discord.utils.get(voice_channels, name=VOICE_CHANNEL_NAME)
+
+    # make VoiceState objects for checking status
+    voice_state_none = discord.VoiceState(data=dict(channel=None))
+    voice_state_in_channel = discord.VoiceState(data=dict(channel=voice_chan_obj))
+
+    print(guilds)
+
 
 @client.event
 async def on_message(message):
@@ -37,4 +54,8 @@ async def on_message(message):
         await sleep(2)
         await message.channel.send('HURRY IT UP')
 
+async def on_voice_state_update(member, ):
+    pass
+
 client.run(TOKEN)
+
