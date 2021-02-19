bfc17d1192f00efbd4326b2128c54b600a5cfcab
Ryley <ryley.g@hotmail.com>
2021-02-18 17:00:28 -0700

    rewrote as a class

--- EggTimerBot.py
+++ EggTimerBot.py
@@ -15,48 +15,49 @@ load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 VOICE_CHANNEL_NAME = os.getenv('VOICE_CHANNEL_NAME')
 
-client = discord.Client()
 
-@client.event
-async def on_connect():
-    # get list of guilds that bot is a member
-    guilds = client.guilds
+class EggTimer(discord.Client):
+    def __init__(self, **options):
+        super().__init__(**options)
+        self.member_obj_list = []
+        self.voice_channels = []
+        self.current_voice_chan = None
+        # make VoiceState objects for checking status
+        self.voice_state_none = discord.VoiceState(data=dict(channel=None))
+        self.voice_state_in_channel = None
 
-    # get list of member objects from list of guilds
-    member_obj_list = [member for guild in guilds for member in guild.members]
+    async def on_connect(self):
+        # get list of member objects from list of guilds
+        self.member_obj_list = [member for guild in self.guilds for member in guild.members]
 
-    # get voice channel for checking state and if anyone is in it
-    voice_channels = [chan for guild in guilds for chan in guild.voice_channels]
-    voice_chan_obj = discord.utils.get(voice_channels, name=VOICE_CHANNEL_NAME)
+        # get VoiceChannel object for checking state and if anyone is in it
+        self.voice_channels = [chan for guild in self.guilds for chan in guild.voice_channels]
+        self.current_voice_chan = discord.utils.get(self.voice_channels, name=VOICE_CHANNEL_NAME)
 
-    # make VoiceState objects for checking status
-    voice_state_none = discord.VoiceState(data=dict(channel=None))
-    voice_state_in_channel = discord.VoiceState(data=dict(channel=voice_chan_obj))
+        # get VoiceState object for selected voice channel
+        self.voice_state_in_channel = discord.VoiceState(data=dict(channel=self.current_voice_chan))
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.lower().startswith('hello'):
-        await message.channel.send('Hello!')
-
-    if 'hurry' in message.content.lower():
-        await message.channel.send(f'{str(message.author).split("#")[0].upper()} SAYS IT IS TAKING TOO LONG')
-        await sleep(2)
-        await message.channel.send('GETTING THE EGG TIMER')
-        await sleep(2)
-        await message.channel.send('STARTING THE EGG TIMER')
-        await sleep(2)
-        await message.channel.send('HURRY IT UP')
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
 
 
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.lower().startswith('hello'):
+            await message.channel.send('Hello!')
+
+        if 'hurry' in message.content.lower():
+            await message.channel.send(f'{str(message.author).split("#")[0].upper()} SAYS IT IS TAKING TOO LONG')
+            await sleep(2)
+            await message.channel.send('GETTING THE EGG TIMER')
+            await sleep(2)
+            await message.channel.send('STARTING THE EGG TIMER')
+            await sleep(2)
+            await message.channel.send('HURRY IT UP')
+
+client = EggTimer()
 
 client.run(TOKEN)
 
