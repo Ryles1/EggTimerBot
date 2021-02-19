7f655e9ca91700eb5c02f1c14c7d90494293a867
Ryley <ryley.g@hotmail.com>
2021-02-19 12:01:13 -0700

    added voice connect and README

--- EggTimerBot.py
+++ EggTimerBot.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 import logging
 from asyncio import sleep
+from sys import platform
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -15,6 +16,8 @@ load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 VOICE_CHANNEL_NAME = os.getenv('VOICE_CHANNEL_NAME')
 
+if platform == "linux" and not discord.opus.is_loaded():
+   discord.opus.load_opus("opus")
 
 class EggTimer(discord.Client):
     def __init__(self, **options):
@@ -33,13 +36,17 @@ class EggTimer(discord.Client):
         # get VoiceChannel object for checking state and if anyone is in it
         self.voice_channels = [chan for guild in self.guilds for chan in guild.voice_channels]
         self.current_voice_chan = discord.utils.get(self.voice_channels, name=VOICE_CHANNEL_NAME)
+        if self.current_voice_chan is None:
+            self.current_voice_chan = discord.utils.get(self.voice_channels, name="General")
 
         # get VoiceState object for selected voice channel
         self.voice_state_in_channel = discord.VoiceState(data=dict(channel=self.current_voice_chan))
 
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
-
+        if self.current_voice_chan is not None:
+            vc = await self.current_voice_chan.connect()
+            print(f'{self.user} has connected to {self.current_voice_chan}')
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -56,6 +63,7 @@ class EggTimer(discord.Client):
             await message.channel.send('STARTING THE EGG TIMER')
             await sleep(2)
             await message.channel.send('HURRY IT UP')
+
 
 client = EggTimer()
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ###### **EGG TIMER BOT**
+Uses the discord.py package to write a simple discord bot.
+
+The bot listens for messages in the channels it is connected to and if any
+message contains the word "hurry", displays an annoying message.
+
+The bot joins the "General" voice channel in a guild and plays an annoying ticking sound.
